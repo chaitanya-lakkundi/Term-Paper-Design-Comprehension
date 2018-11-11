29b3ad89b7919976d5303150adcbdf10ce02a5eb
chaitanya-lakkundi <chaitanya.lakkundi@gmail.com>
2018-11-11 14:05:56 +0530

    add pagination capability

--- get_commits_github.py
+++ get_commits_github.py
@@ -1,25 +1,42 @@
 from get_emails_github import *
 
+def forwardCommitPage(page_tree):
+    try:
+        sel = CSSSelector(".pagination")
+        return sel(page_tree)[0][1].get("href")
+    except Exception as e:
+        return None
+
 def getCommits(user_project_name):
     url = "https://github.com/" + user_project_name + "/commits/master"
     print(url)
-    r = requests.get(url)
-    tree = html.fromstring(r.text)
-    sel = CSSSelector("p.commit-title a.message")
-    commit_links = [commit_href.get('href') for commit_href in sel(tree)]
+    commit_links = []
+    for _ in range(2):
+        r = requests.get(url)
+        tree = html.fromstring(r.text)
+        sel = CSSSelector("p.commit-title a.message")
+        commit_links += [commit_href.get('href') for commit_href in sel(tree)]
+        url = forwardCommitPage(tree)
+        if not url:
+            break
     return commit_links
 
 def getCommitMetadata(link):
     r = requests.get(link)
     tree = html.fromstring(r.text)
+
+    commit_date_sel = CSSSelector(".commit-meta relative-time")
+    commit_date = commit_date_sel(tree)[0].get("datetime")
+
     changed_filename_sel = CSSSelector("div.file-info a.link-gray-dark")
     changes_filenames = [ele.text for ele in changed_filename_sel(tree)]
-    print(changes_filenames)
+
+    print(commit_date,changes_filenames)
 
 def main(choices):
     try:
-        for line in choices:
-            commits = getCommits(line.strip())
+        for project_name in choices:
+            commits = getCommits(project_name.strip())
             print("Scraping Metadata for "+str(len(commits))+" commits")
             for commit_href in commits:
                 getCommitMetadata("https://github.com"+commit_href)
